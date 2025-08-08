61a094565cce47af56482bb85e34f667ba22c32d
Kartikey <kk8174569@gmail.com>
2025-08-08 19:36:51 +0530

    init commit

---
+++ src/Employee.java
@@ -0,0 +1,14 @@
+public class Employee {
+    int id;
+    String name;
+
+    Employee(int i, String n)   // Constructor
+    {
+        id=i;
+        name=n;
+    }
+    void display()
+    {
+        System.out.println("Id "+id+" Name "+name);
+    }
+}
