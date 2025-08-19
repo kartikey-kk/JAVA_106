fa3baed054695012ee2284e4a09bffc32efbd187
Kartikey <kk8174569@gmail.com>
2025-08-19 12:07:10 +0530

    init commit

--- src/A.java
+++ src/A.java
@@ -1,6 +1,23 @@
+//public class A {
+//    // static variable
+//
+//    static int bread=10;
+//    static {
+//        System.out.println("I am in class A");
+//    }
+//    public static void eat(){
+//        bread--;
+//    }
+//    public static void display(){
+////        //int x;
+//          System.out.println("slices Remaining"+bread);
+////        //System.out.println(x);
+//    }
+//}
+
 public class A {
-    static int bread=10;
-    public void display(){
-        System.out.println("slices Remaining"+bread);
-    }
+        public void display(String x){
+            System.out.println("I am in class A"+x);
+        }
+
 }
---
+++ src/B.java
@@ -0,0 +1,5 @@
+public class B extends A{
+    public void display(){
+        System.out.println("I am in class B");
+    }
+}
