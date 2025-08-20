3c3f31eb2cd6764281be05ab78fc5ae229c0f8bd
Kartikey <kk8174569@gmail.com>
2025-08-20 14:00:59 +0530

    init commit

--- src/A.java
+++ src/A.java
@@ -16,7 +16,7 @@
 //}
 
 public class A {
-        public void display(String x){
+        public final void display(String x){
             System.out.println("I am in class A"+x);
         }
 
---
+++ src/MyMain1.java
@@ -0,0 +1,8 @@
+public class MyMain1 {
+
+    public  static void main(String[] args){
+        collegeStudent cs=new collegeStudent("abhi",12);
+        cs.attend_class();
+        cs.give_exam();
+    }
+}
