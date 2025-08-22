eb8f38c852f8b155460e2cea323d5c6b17ebc743
Kartikey <kk8174569@gmail.com>
2025-08-22 11:50:09 +0530

    22 Aug 2025

---
+++ src/MyStudent2.java
@@ -0,0 +1,9 @@
+import java.sql.SQLOutput;
+
+public class MyStudent2 {
+    public static void main(String[] args) {
+        Student2 st =new Student2("A",12,"Rahul");
+        System.out.println(st.name);
+        System.out.println(st.getGrade());
+    }
+}
