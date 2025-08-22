4bb6c6b8575efce9dd6b07839c09c08cb7030535
Kartikey <kk8174569@gmail.com>
2025-08-22 12:35:47 +0530

    22 Aug 2025

---
+++ src/MyPerson.java
@@ -0,0 +1,9 @@
+public class MyPerson {
+    public static void main(String[] args) {
+        Person p=new Person("Ghaziabad");
+        p.getAddress();
+        schoolStudent st=new schoolStudent("Ghaziabad","B.tech");
+        st.getAddress();
+        st.getCourse();
+    }
+}
