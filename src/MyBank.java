7221f62b405287e94fab6bc4f2ffd3408653e6de
Kartikey <kk8174569@gmail.com>
2025-08-29 11:46:07 +0530

    29 Aug 2025 InsufficientBalanceException Handling

---
+++ src/MyBank.java
@@ -0,0 +1,15 @@
+public class MyBank {
+    public static void main(String[] args) {
+        Bank b =new Bank();
+
+        try
+        {
+            b.withdrawal(100000);
+        }
+       catch (InsufficientBalanceException e)
+       {
+           System.out.println(e.getMessage());
+       }
+
+    }
+}
